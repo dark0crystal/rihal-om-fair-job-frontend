db288d1da1e10cb8d123d474b5a3c346e188a9d8
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-20 11:54:45 +0400

    adding the moving button effect

--- src/components/navbar/Button.tsx
+++ src/components/navbar/Button.tsx
@@ -1,19 +1,45 @@
 "use client"
-
+import {motion} from "framer-motion"
 type button = {
     isActive:boolean,
     toggleMenu :any
 }
 export default function Button({isActive , toggleMenu}:button){
     return(
-        <div className="">
-            <div>
-                {isActive ? (
-                    <div onClick={() => {toggleMenu()}}>clese</div>
-                ):(
-                    <div onClick={() => {toggleMenu()}}>open</div>
-                )}
+            <div className="absolute top-12 right-9 w-[100px] h-[40px] overflow-hidden">
+                <motion.div
+                className="relative w-full h-full   bg-black"
+                animate={{top:isActive ? "-100%" : "0%"}}
+                >
+                    <div 
+                    className=" w-full h-full bg-amber-200"
+                    onClick={() => {toggleMenu()}}
+                    >
+                         <PerspectiveText label="Menu"/>
+                    </div>
+
+                    <div 
+                    className="w-full h-full bg-red-300"
+                    onClick={() => {toggleMenu()}}
+                    >
+                         <PerspectiveText label="Menu"/>
+                    </div>
+        
+                </motion.div>
             </div>
+       
+    )
+}
+
+type perspectiveText={
+    label :string
+}
+
+function PerspectiveText({label}:perspectiveText) {
+    return (    
+        <div className=" flex justify-center items-center">
+            <p>{label}</p>
+            <p>{label}</p>
         </div>
     )
 }

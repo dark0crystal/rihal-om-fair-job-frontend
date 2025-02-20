61ce5aec58f500d6fe1ea7607c29ad3b0f5306cd
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-20 15:06:53 +0400

    remove unused

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <div className="bg-[#FEF3E2]">
-        <div>
-          <h1 className="text-black text-[200px] font-bold">
-            RIHAL CHALLENGE
-          </h1>
-        </div>
-        <div className="h-[100vh]"/>
-      </div>
+     <div>index</div>
   );
 }

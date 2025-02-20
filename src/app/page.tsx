fb7da54c105f2ae328eafc28336967e25b193dd4
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-20 19:49:06 +0400

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
+
 import SummarizerForm from "@/components/form/SummarizerForm";
-import Image from "next/image";
 
 export default function Home() {
   return (
-     <div className="h-[100vh] w-screen flex justify-center items-center">
-      <SummarizerForm/>
+     <div>
+      
+      <div  className="h-[100vh] w-screen flex justify-center items-center">
+      <div>
+        <SummarizerForm/>
+      </div>
+      </div>
+      <div className="h-[100vh] w-screen">
+
+      </div>
      </div>
   );
 }

b942822eb7207e68b09ab000b9322bcc0c5104d6
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-20 13:38:14 +0400

    design the button

--- src/components/navbar/Button.tsx
+++ src/components/navbar/Button.tsx
@@ -6,7 +6,7 @@ type button = {
 }
 export default function Button({isActive , toggleMenu}:button){
     return(
-            <div className="absolute top-12 right-9 w-[100px] h-[40px]  overflow-hidden">
+            <div className="absolute top-12 right-9 w-[100px] h-[40px] rounded-3xl  overflow-hidden cursor-pointer">
                 <motion.div
                 className="relative w-full h-full   bg-black"
                 animate={{top:isActive ? "-100%" : "0%"}}
@@ -15,7 +15,7 @@ export default function Button({isActive , toggleMenu}:button){
                     className=" w-full h-full bg-amber-200"
                     onClick={() => {toggleMenu()}}
                     >
-                         <PerspectiveText label="Menu"/>
+                         <PerspectiveText  label="Menu"/>
                     </div>
 
                     <div 
@@ -37,9 +37,9 @@ type perspectiveText={
 
 function PerspectiveText({label}:perspectiveText) {
     return (    
-        <div className=" flex justify-center items-center w-full h-full">
-            <p>{label}</p>
-            <p>{label}</p>
+        <div className=" text-black flex justify-center items-center w-full h-full hover:rotate-x-[90deg]  transform-3d translate-x-[0.76, 0, 0.24, 1] duration-75">
+            <p className=" hover:opacity-0   hover:translate-y-[-100%]">{label}</p>
+            <p className="absolute   opacity-0 hover:opacity-100 ">{label}</p>
         </div>
     )
 }

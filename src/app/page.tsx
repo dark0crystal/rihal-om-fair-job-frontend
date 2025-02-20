59fcaf90caad3a4db5f0864e55b935b9f833213d
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-20 16:49:31 +0400

    design index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-     <div>
+     <div className="h-[100vh] w-screen flex justify-center items-center">
       <SummarizerForm/>
      </div>
   );

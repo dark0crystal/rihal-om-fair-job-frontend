4dcd4b42c1cc5cf128cad66bc6d8cdd943a03e5d
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-20 14:58:10 +0400

    fix the button position

--- src/components/navbar/Button.tsx
+++ src/components/navbar/Button.tsx
@@ -6,20 +6,20 @@ type button = {
 }
 export default function Button({isActive , toggleMenu}:button){
     return(
-            <div className="absolute top-12 right-9 w-[100px] h-[40px] rounded-3xl  overflow-hidden cursor-pointer">
+            <div className="absolute top-0 right-0 w-[100px] h-[40px] rounded-3xl  overflow-hidden cursor-pointer">
                 <motion.div
                 className="relative w-full h-full   bg-black"
                 animate={{top:isActive ? "-100%" : "0%"}}
                 >
                     <div 
-                    className=" w-full h-full bg-amber-200"
+                    className=" w-full h-full bg-[#c9fd74]"
                     onClick={() => {toggleMenu()}}
                     >
                          <PerspectiveText  label="Menu"/>
                     </div>
 
                     <div 
-                    className="w-full h-full bg-red-300"
+                    className="w-full h-full bg-black text-white"
                     onClick={() => {toggleMenu()}}
                     >
                          <PerspectiveText label="Close"/>
@@ -37,7 +37,7 @@ type perspectiveText={
 
 function PerspectiveText({label}:perspectiveText) {
     return (    
-        <div className=" text-black flex justify-center items-center w-full h-full hover:rotate-x-[90deg]  transform-3d translate-x-[0.76, 0, 0.24, 1] duration-75">
+        <div className="  flex justify-center items-center w-full h-full hover:rotate-x-[90deg]  transform-3d translate-x-[0.76, 0, 0.24, 1] duration-75">
             <p className=" hover:opacity-0   hover:translate-y-[-100%]">{label}</p>
             <p className="absolute   opacity-0 hover:opacity-100 ">{label}</p>
         </div>

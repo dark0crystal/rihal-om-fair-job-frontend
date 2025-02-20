689b3a2edd93eb68103aa39b9657346f6ac34765
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-20 11:10:06 +0400

    pass the state to toggle menu button

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,10 +1,18 @@
+"use client"
 import React from 'react'
 import Brand from './Brand'
+import Button from './Button'
+import { useState } from 'react'
 
 const NavBar = () => {
+
+  const [isActive, setActive] = useState(false)
   return (
     <div className='w-screen h-[12vh] flex justify-center items-center'>
-       <Brand />
+       {/* <Brand /> */}
+       <div>
+          <Button isActive={isActive} toggleMenu={()=>{setActive(!isActive)}} />
+       </div>
     </div>
   )
 }

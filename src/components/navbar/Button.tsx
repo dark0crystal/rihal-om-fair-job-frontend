58fa506f439a6c6aa83fbf871ca3f476f796fe5e
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-20 11:59:48 +0400

    center the buttons text

--- src/components/navbar/Button.tsx
+++ src/components/navbar/Button.tsx
@@ -6,7 +6,7 @@ type button = {
 }
 export default function Button({isActive , toggleMenu}:button){
     return(
-            <div className="absolute top-12 right-9 w-[100px] h-[40px] overflow-hidden">
+            <div className="absolute top-12 right-9 w-[100px] h-[40px]  overflow-hidden">
                 <motion.div
                 className="relative w-full h-full   bg-black"
                 animate={{top:isActive ? "-100%" : "0%"}}
@@ -22,7 +22,7 @@ export default function Button({isActive , toggleMenu}:button){
                     className="w-full h-full bg-red-300"
                     onClick={() => {toggleMenu()}}
                     >
-                         <PerspectiveText label="Menu"/>
+                         <PerspectiveText label="Close"/>
                     </div>
         
                 </motion.div>
@@ -37,7 +37,7 @@ type perspectiveText={
 
 function PerspectiveText({label}:perspectiveText) {
     return (    
-        <div className=" flex justify-center items-center">
+        <div className=" flex justify-center items-center w-full h-full">
             <p>{label}</p>
             <p>{label}</p>
         </div>

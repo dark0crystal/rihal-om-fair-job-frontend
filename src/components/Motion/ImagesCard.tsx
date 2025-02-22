7f2c976935bef0612b4a7129373dc5596d83811c
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-22 17:44:13 +0400

    add animation

--- src/components/Motion/ImagesCard.tsx
+++ src/components/Motion/ImagesCard.tsx
@@ -1,16 +1,28 @@
+"use client"
 import Image from "next/image"
-import { Key } from "react"
-
+import {motion} from "framer-motion"
+import { useScroll,useTransform} from "framer-motion";
+import { useRef } from "react";
+import { Key } from "react";
 export default function ImagesCard( {cardInfo, customStyle , bgColor}:any){
 
-    // const ImagesArr = [
-    //     {img:},
-        
-    // ]
-    return(
-        <div className={`h-fit w-[150px] bg-white flex flex-col p-3 items-center ${bgColor}`}>
+    const targetRef = useRef<HTMLDivElement>(null);
+    const {scrollYProgress} = useScroll({
+        target:targetRef , 
+        offset:["start end" , "end start"]
+    })
+    const y =useTransform ( scrollYProgress, [0,1] , [-100,150] )
+    const left = "-55%";
 
-            <div>
+    return(
+        <motion.div ref={targetRef}
+        style={{y}}
+        className={` h-fit w-[150px] bg-white flex flex-col p-3 items-center ${bgColor}`}>
+
+            <div
+            className=""
+           
+            >
                 {cardInfo.map((img: any , index: Key | null | undefined)=>(
                     <div key={index} className={`w-[130px] h-[130px] relative my-2 ${customStyle}`}>
                         <Image  src={img} alt="image" objectFit="cover" fill/>
@@ -18,6 +30,6 @@ export default function ImagesCard( {cardInfo, customStyle , bgColor}:any){
                 ))}
             </div>
            
-        </div>
+        </motion.div>
     )
 }

d293611dc4fd40f5fabc3824fd3368a8d6a31add
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-22 16:26:33 +0400

    adding motion to the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   const targetRef = useRef<HTMLDivElement>(null);
   const {scrollYProgress} = useScroll({
     target:targetRef,
-    offset:["start end","start end"]
+    offset:["start start","end start"]
   })
 // Default: ["start start", "end end"]
 // offset describes intersections, points where the target and container meet.
@@ -23,33 +23,36 @@ export default function Home() {
 // So if the target is an element, the container is the window, 
 // and we're tracking the vertical axis then "start end" is where the top of the element meets the bottom of the viewport.
 
-  const scale= useTransform(scrollYProgress , [0,0.5], [1,0.5])
+  const scale= useTransform(scrollYProgress , [1,0], [0.7, 1])
   return (
      <div ref={targetRef} className="overflow-hidden"> 
-      <div  className=" relative h-[80vh] w-screen flex flex-col justify-center items-center bg-[#f9f5ec]">
-        {/* floating images section */}
-        <motion.div 
-        className="absolute top-8 left-20 -z-0"
-        style={{scale}}
-        >
-          <ImagesCard cardInfo={first} customStyle="" bgColor=""/>
-        </motion.div>
-        <div className="z-10 w-screen flex flex-col justify-center items-center h-[40vh] text-[#131313] text-center">
+      <div className="h-[80vh] w-screen flex flex-col justify-center items-center bg-[#f9f5ec]">
+        {/* Hero Paragraph */}
+        <motion.div style={{scale}} className="p-10 lg:p-2 w-screen flex flex-col justify-center items-center md:min-h-[30vh] min-h-[40vh] h-fit text-[#131313] text-center">
             <Image src={Ximg} width={200} height={150} alt="xx"/>
-            <h1 className=" text-8xl  font-semibold "><span className="text-[#7546ff]">Ready</span> to get insightful summaries?<span className="text-[#7546ff]"> Let&apos;s dive in</span></h1>   
-        </div>
+            <h1  className="text-3xl md:text-6xl lg:text-8xl font-bold   lg:font-semibold "><span className="text-[#7546ff]">Ready</span> to get insightful summaries?<span className="text-[#7546ff]"> Let&apos;s dive in</span></h1>   
+        </motion.div>
+        {/* Form */}
       <div className="mt-10">
         <SummarizerForm/>
       </div>
       </div>
+      {/*  */}
       <div>
         <TextCard cardInfo={firstContent}/>
       </div>
+      {/*  */}
       <div className="h-[100vh] w-screen bg-[#f9f5ec]">
           <ImagesCard cardInfo={first} customStyle="" bgColor=""/>
           <ImagesCard cardInfo={first} customStyle="" bgColor=""/>
           
       </div>
+       {/* floating images section */}
+       <motion.div 
+        className=""
+        >
+          <ImagesCard cardInfo={first} customStyle="" bgColor=""/>
+        </motion.div>
 
      </div>
   );

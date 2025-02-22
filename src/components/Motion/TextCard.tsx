0dd58c28745684aafb8a07b35286860581274ead
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-22 17:44:35 +0400

    add animation

--- src/components/Motion/TextCard.tsx
+++ src/components/Motion/TextCard.tsx
@@ -1,15 +1,28 @@
+"use client"
+import {motion} from "framer-motion"
+import { useScroll,useTransform} from "framer-motion";
+import { useRef } from "react";
 import { Key } from "react";
 
 export default function TextCard( {cardInfo, customStyle , bgColor}:any){
+    const targetRef = useRef<HTMLDivElement>(null);
+    const {scrollYProgress} = useScroll({
+        target:targetRef , 
+        offset:["start end" , "end start"]
+    })
+    const x =useTransform ( scrollYProgress, [0,1] , [-100,150] )
+    const left = "-55%";
     return(
-        <div>
-            <div className="flex flex-row p-4 items-center justify-center bg-[#f8fe23]  h-[100px] overflow-hidden">
+        <div ref={targetRef}>
+            <motion.div  className="flex flex-row p-4 items-center justify-center bg-[#f8fe23]  h-[100px] overflow-hidden">
                 {[...cardInfo,...cardInfo].map((content: string , index: Key | null | undefined)=>(
-                    <div key={index} className={`mx-8  ${customStyle}`}>
+                    <motion.div 
+                    style={{left , x}}
+                     key={index} className={`mx-8  ${customStyle}`}>
                         <h1 className="text-3xl font-semibold">{content}</h1>
-                    </div>
+                    </motion.div>
                 ))}
-            </div>
+            </motion.div>
         </div>
     )
 }

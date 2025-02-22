d64a5df30d474e53335a5b7c11785aa51328bea2
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-22 12:06:19 +0400

    add the xxx.png in index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 
 import SummarizerForm from "@/components/form/SummarizerForm";
-
+import Image from "next/image";
+import Ximg from "../../public/xxx.png"
 
 export default function Home() {
   return (
      <div>
       
-      <div  className="h-[80vh] w-screen flex flex-col justify-center items-center bg-black">
-        <div className="h-fit w-screen text-white">
-            <h1 className="text-9xl font-semibold ">WOW</h1>
-            <h1 className="text-9xl font-semibold">WOW</h1>
-            <h1 className="text-9xl font-semibold">WOW</h1>
+      <div  className="h-[80vh] w-screen flex flex-col justify-center items-center bg-[#f9f5ec]">
+        <div className=" w-screen flex flex-col justify-center items-center h-[40vh] text-[#131313] text-center">
+            <Image src={Ximg} width={200} height={150} alt="xx"/>
+            <h1 className=" text-8xl  font-semibold "><span className="text-[#7546ff]">Ready</span> to get insightful summaries?<span className="text-[#7546ff]"> Let&apos;s dive in</span></h1>   
         </div>
       <div>
         <SummarizerForm/>
@@ -23,3 +23,11 @@ export default function Home() {
      </div>
   );
 }
+
+// #f8fe23 green
+
+// #7546ff purple
+
+// #131313 black
+
+// #f9f5ec near to white

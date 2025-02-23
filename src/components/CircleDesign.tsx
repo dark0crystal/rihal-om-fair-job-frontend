8492211557974f85a37bf066f1dc4003032f2c5b
ALMardasDEV <albusaidi9094@gmail.com>
2025-02-23 23:07:11 +0400

    design the start circle

---
+++ src/components/CircleDesign.tsx
@@ -0,0 +1,17 @@
+
+
+export default function CircleDesign({bgColor, innerColor ,width ,height }:any){
+
+  return(
+    <div className={`${bgColor}  ${width} ${height} rounded-full grid grid-cols-2 grid-rows-2 overflow-hidden ` }>
+        <div className="min-w-[30px] min-h-[30px] bg-amber-200 rounded-br-4xl"/>
+        <div className="min-w-[30px] min-h-[30px] bg-amber-200 rounded-bl-4xl"/>
+        <div className="min-w-[30px] min-h-[30px] bg-amber-200 rounded-tr-4xl"/>
+        <div className="min-w-[30px] min-h-[30px] bg-amber-200 rounded-tl-4xl"/>
+        {/* <div className={`${innerColor} ${width} ${height} rounded-full `}>
+
+        </div> */}
+
+    </div>
+  )
+}
